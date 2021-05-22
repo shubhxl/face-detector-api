76f4b1e91e515b473a8025c82fe3e5da8c3f4253
shubhxl <shubhamsinha1997@gmail.com>
2021-05-22 19:52:33 +0530

    Secured API Key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: '868f58b0dcaf4e099f629cd3c13fa93b'
+    apiKey: process.env.API_CLARIFAI
    });
 
 const handleApiCall = (req, res) => {
